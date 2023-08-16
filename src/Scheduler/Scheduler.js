ce63cf28613b27bdca2df875e1d3c54403eeb3ad
“Gauthier <gauthier.willemse@gmail.com>
2023-08-16 21:38:04 +0200

    First part: <8 weeks, dtp

--- src/Scheduler/Scheduler.js
+++ src/Scheduler/Scheduler.js
@@ -1,11 +1,57 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
 import { AiFillDelete } from 'react-icons/ai';
-import { vaccineOptions }from './Variables';
+import { vaccineOptions } from './Variables';
 import './Scheduler.css';
+
+const calculateAge = dob => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    let ageYears = today.getFullYear() - birthDate.getFullYear();
+    let ageMonths = today.getMonth() - birthDate.getMonth();
+    let ageWeeks = Math.floor((today.getDate() - birthDate.getDate()) / 7);
+
+    if (ageMonths < 0 || (ageMonths === 0 && today.getDate() < birthDate.getDate())) {
+        ageYears--;
+        ageMonths += 12;
+    }
+
+    return { ageYears, ageMonths, ageWeeks };
+};
+
+const getVaccineSchedule = (dob, vaccine) => {
+    const { ageYears, ageMonths, ageWeeks } = calculateAge(dob);
+    const schedules = [];
+
+    const totalAgeInWeeks = ageYears * 52 + ageMonths * 4 + ageWeeks;
+
+    if (vaccine.name === 'DTPa-VHB-IPV-Hib' && totalAgeInWeeks < 8) {
+        const intervals = [8, 12, 16, 15 * 4.3];
+        intervals.forEach(weeks => {
+            // Calculate the exact target date
+            const targetDate = new Date(new Date(dob).getTime() + weeks * 7 * 24 * 60 * 60 * 1000);
+
+            // Calculate the range (+- 5 days)
+            const startDate = new Date(targetDate.getTime() - 5 * 24 * 60 * 60 * 1000);
+            const endDate = new Date(targetDate.getTime() + 5 * 24 * 60 * 60 * 1000);
+
+            schedules.push({ startDate, targetDate, endDate });
+        });
+    }
+
+    return schedules;
+};
+
+const formatDate = (date) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString(undefined, options);
+};
+
 
 const Scheduler = () => {
     const [dob, setDob] = useState("");
+    const [age, setAge] = useState({ ageYears: null, ageMonths: null, ageWeeks: null });
+    const [error, setError] = useState(null);
     const [vaccines, setVaccines] = useState([
         {
             name: '',
@@ -47,16 +93,45 @@ const Scheduler = () => {
         setVaccines(newVaccines);
     };
 
+    const [vaccineSchedule, setVaccineSchedule] = useState([]);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        if (!dob) {
+            setError("Input age");
+            return;
+        }
+
+        const selectedVaccine = vaccines[0];
+        const isDTPaVaccine = selectedVaccine.name === 'DTPa-VHB-IPV-Hib';
+        const childAge = calculateAge(dob);
+        const totalAgeInWeeks = childAge.ageYears * 52 + childAge.ageMonths * 4 + childAge.ageWeeks;
+        const noDosesGiven = selectedVaccine.doses.every(dose => !dose.date);
+
+        if (isDTPaVaccine && totalAgeInWeeks < 8 && noDosesGiven) {
+            const schedules = getVaccineSchedule(dob, selectedVaccine);
+            setVaccineSchedule(schedules);
+        } else {
+            setVaccineSchedule([]);
+        }
+
+        setAge(childAge);
+        setError(null);
+        console.log("Selected Vaccine:", selectedVaccine.name);
+        console.log("Child's age in weeks:", totalAgeInWeeks);
+    };
+
+    console.log('Vaccination schedule: ', vaccineSchedule)
 
     return (
         <div className="Scheduler">
             <h1>Vaccination Scheduler</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="input-field">
                     <label>Date of Birth:</label>
                     <input type="date" value={dob} onChange={event => setDob(event.target.value)} />
                 </div>
-
                 <div className="input-field">
                     <label>Add vaccinations:</label>
                     {vaccines.map((vaccine, index) => (
@@ -71,33 +146,46 @@ const Scheduler = () => {
                                     />
                                     <AiFillDelete className="removeicon" onClick={() => removeVaccine(index)} />
                                 </div>
-
                             </div>
-
-                            {vaccine.doses.map((dose, doseIndex) => (
-                                <div key={doseIndex}>
-                                    <label>Dose {doseIndex + 1}:</label>
-                                    <div className='dosedate' >
-                                        <input
-                                            type="date"
-                                            value={dose.date}
-                                            onChange={event => handleDoseDateChange(index, doseIndex, event)}
-                                        />
-                                        <AiFillDelete className='removeicon' onClick={() => removeDose(index, doseIndex)} />
+                            <div className="doses-container">
+                                {vaccine.doses.map((dose, doseIndex) => (
+                                    <div className="dose-item" key={doseIndex}>
+                                        <label>Dose {doseIndex + 1}:</label>
+                                        <div className='dosedate'>
+                                            <input
+                                                type="date"
+                                                value={dose.date}
+                                                onChange={event => handleDoseDateChange(index, doseIndex, event)}
+                                            />
+                                            <AiFillDelete className='removeicon' onClick={() => removeDose(index, doseIndex)} />
+                                        </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                             <button type="button" onClick={() => addDose(index)}>Add Dose</button>
                         </div>
                     ))}
                     <button type="button" onClick={addVaccine}>Add Vaccine</button>
                 </div>
+                <input type="submit" value="Calculate Schedule" className="submit-btn" />
+                {error &&
+                    <p className="error-message">
+                        {error}
+                    </p>
+                }
+                {age.ageYears !== null &&
+                    <p className="age-display">
+                        Age: {age.ageYears} years, {age.ageMonths} months, and {age.ageWeeks} weeks
+                    </p>
+                }
+                {vaccineSchedule.map((schedule, index) => (
+                    <p key={index} className="vaccine-date">
+                        Recommended date for dose {index + 1}: {formatDate(schedule.startDate)} to {formatDate(schedule.endDate)}
+                    </p>
+                ))}
 
-                <input type="submit" value="Calculate Schedule" />
             </form>
         </div>
     );
-
 }
-
 export default Scheduler;
